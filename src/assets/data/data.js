e0cbbdbc33e7df253631023febc7537836ac1e29
aliwat23 <159437044+aliwat23@users.noreply.github.com>
2024-03-04 00:20:11 +0100

    Add files via upload

---
+++ src/assets/data/data.js
@@ -0,0 +1,96 @@
+export const nav = [
+  {
+    id: 1,
+    text: "home",
+    url: "/",
+  },
+  {
+    id: 2,
+    text: "about",
+    url: "/about",
+  },
+
+  {
+    id: 5,
+    text: "contact",
+    url: "/contact",
+  },
+]
+export const category = [
+  {
+    id: 1,
+    category: "Life",
+    title: "Stay Calm And Surf",
+    cover: "../images/category/ca1.png",
+  },
+  {
+    id: 2,
+    category: "Fashion",
+    title: "Becoming a Dragonfly",
+    cover: "../images/category/ca2.png",
+  },
+  {
+    id: 3,
+    category: "Tech",
+    title: "Everything you need to be successful online",
+    cover: "../images/category/ca.png",
+  },
+  {
+    id: 4,
+    category: "Sport",
+    title: "Stay Calm And Surf",
+    cover: "../images/category/cvv.jpg",
+  },
+  {
+    id: 5,
+    category: "Food",
+    title: "Foodi, pour vos pauses repas plaisir. Gérez vos dépenses de restauration et votre compte en toute autonomie n'a jamais été aussi facile !",
+    cover: "../images/category/caa.jpeg",
+  },
+  {
+    id: 6,
+    category: "Health",
+    title: "Becoming a Dragonfly",
+    cover: "../images/category/ca6.png",
+  },
+  {
+    id: 7,
+    category: "Business",
+    title: "Stay Calm And Surf",
+    cover: "../images/category/ca7.png",
+  },
+  {
+    id: 8,
+    category: "Technology",
+    title: "There's always light at the end of the tunnel",
+    cover: "../images/category/ca8.png",
+  },
+]
+export const blog = [
+  {
+    id: 1,
+    title: "Hacking the cybersecurity skills gap in developing countries",
+    desc: "Quisque at tellus nec magna viverra euismod. Maecenas lobortis, nisl et sodales blandit, neque mi ullamcorper eros, id efficitur ex velit in ante. Suspendisse elementum congue lacus quis gravida. Nunc eu hendrerit risus. Quisque blandit, orci non maximus vulputate, ligula ante ornare enim, ac interdum lorem leo varius mauris. Aliquam non sem vel orci malesuada sagittis. Vivamus id ullamcorper tellus. In hac habitasse platea dictumst. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras et sapien a arcu feugiat mattis. Suspendisse sed magna quis turpis rhoncus venenatis.",
+    category: "Digital",
+    cover: "../images/blogs/b1.webp",
+    date: "03, 2024",
+  },
+
+  {
+    id: 2,
+    title: "LeBron James becomes first player in NBA history to reach 40,000 points",
+    desc: "Saturday's feat was James' first significant scoring milestone since passing Hall of Famer Kareem Abdul-Jabbar to become the league's all-time leading scorer roughly a year ago. Before that, he became just the seventh player to surpass 30,000 points.",
+    category: "Sport",
+    cover: "../images/blogs/th.jpeg",
+    date: "02, 2024",
+  },
+  {
+    id: 3,
+    title: "Tanjia Marrakchia Tangia (cuisine marocaine)",
+    desc: "Tanjia Marrakchia Tangia (cuisine marocaine) cuite au barbecue. Je vous reviens avec la recette d’un plat traditionnel de la cuisine marocaine connu sous le nom de la Tanjia",
+    category: "Food",
+    cover: "../images/blogs/be2.jpg",
+    date: "02, 2024",
+  },
+  
+]
